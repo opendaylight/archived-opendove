16ec26efbc7b73fca3d0244d1cecaa7c04dc66ca
Anees Shaikh <aasdevaddr@gmail.com>
2014-01-19 20:11:12 -0800

    Updated ovs-agent build files and copyright/license. Updated build script for third-party components Updated main README file with more details on building
    Signed-off-by: Anees Shaikh <aasdevaddr@gmail.com>

--- ovs-agent/dove-dmc-client.c
+++ ovs-agent/dove-dmc-client.c
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2013 IBM Corporation
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Source File:
+ *      dove-dmc-client.c
+ *      REST client for interaction with ODMC 
+ *
+ *  Author:
+ *      Liran Schour
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- ovs-agent/dove-dmc-client.h
+++ ovs-agent/dove-dmc-client.h
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2013 IBM Corporation
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Source File:
+ *      dove-dmc-client.h
+ *
+ *
+ *  Author:
+ *      Liran Schour
+ */
+
+
 #ifndef DOVE_DMC_CLIENT_H
 #define DOVE_DMC_CLIENT_H 1
 
